47e050486f4448c665a4f132a4cfae7e52a3b6ee
rharnls72 <fnladl72@naver.com>
2016-04-15 15:24:14 +0900

    change to public

--- src/main/java/phonerate/App.java
+++ src/main/java/phonerate/App.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class App {
 	private static final Logger log = LoggerFactory.getLogger(App.class);
-	private App(){
+	public App(){
 	}
 	public static void main( String[] args ){
 		
