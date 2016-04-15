50e1b05f3bf580e3994a472701fbfb513e04070d
rharnls72 <fnladl72@naver.com>
2016-04-15 14:18:47 +0900

    delete field

--- src/main/java/phonerate/App.java
+++ src/main/java/phonerate/App.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class App {
 	private static final Logger log = LoggerFactory.getLogger(App.class);
-	private App(){
-	}
 	public static void main( String[] args ){
 		
 		TotalRate totalrate;
